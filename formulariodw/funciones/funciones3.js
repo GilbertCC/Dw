a5081ed85909fdea695d8d5995cccd8e4968176f
Gilbert S Cachaya Caicedo <47873666+GilbertCC@users.noreply.github.com>
2019-04-10 13:31:21 -0500

    Update funciones3.js

--- formulariodw/funciones/funciones3.js
+++ formulariodw/funciones/funciones3.js
@@ -29,39 +29,37 @@ menor = parseInt($("#menor").val());
 var myTableDiv = document.getElementById("metric_results")
 var table = document.createElement('TABLE')
 var tableBody = document.createElement('TBODY')
-var datos = [];
+var datosx = ["secuencias"];
+var datosY = ["Valores de x","Valores de Y ", "Valores de z", 1];
 table.border = '1'
 table.appendChild(tableBody);
 
-var heading = new Array();
-heading[0] = "secuencia"
-for( i = 1 ; i >= mayor;  i++  ){
-    heading[i] = "" + i
-}
+ for (i = menor ; i < mayor+1 ;  i++){
+     datosx.push(i)
 
+ }
+alert(datosx)
 
-var stock = new Array()
-stock[0] = new Array("valores de x", "88.625", "85.50", "85.81", "987")
-stock[1] = new Array("valores de y", "88.625", "85.50", "85.81", "989")
-stock[2] = new Array("valores de z", "88.625", "85.50", "85.81", "990")
+  
 
 
 //COLUMNAS DE LA TABLA
 var tr = document.createElement('TR');
 tableBody.appendChild(tr);
-for (i = 0; i < heading.length; i++) {
+for (i = 0; i < datosx.length; i++) {
     var th = document.createElement('TH')
     th.width = '75';
-    th.appendChild(document.createTextNode(heading[i]));
+    th.appendChild(document.createTextNode(datosx[i]));
     tr.appendChild(th);
 }
 
 //FILAS DE LA TABLA
-for (i = 0; i < stock.length; i++) {
+for (i = 0; i < 4 ; i++) {
     var tr = document.createElement('TR');
-    for (j = 0; j < stock[i].length; j++) {
+    
+    for (j = 0; j < datosY.length; j++) {
         var td = document.createElement('TD')
-        td.appendChild(document.createTextNode(stock[i][j]));
+        td.appendChild(document.createTextNode(datosY[i]));
         tr.appendChild(td)
     }
     tableBody.appendChild(tr);
