411be1b8d464ea2e31b8af8e39de31eaf87069ea
GilbertCC <47873666+GilbertCC@users.noreply.github.com>
2019-04-09 23:21:07 -0500

    revisar

--- formulariodw/funciones/funciones3.js
+++ formulariodw/funciones/funciones3.js
@@ -22,17 +22,52 @@ if($("#menor").val() == ""){
 
 mayor = parseInt($("#mayor").val());
 menor = parseInt($("#menor").val());
-d
+
 
 //tabla 
 
-var t = "<table>";
-for (i<= 0;i>mayor; i++){
-    t += "<tr> +i";
-    t*= "</tr>"
+var myTableDiv = document.getElementById("metric_results")
+var table = document.createElement('TABLE')
+var tableBody = document.createElement('TBODY')
+var datos = [];
+table.border = '1'
+table.appendChild(tableBody);
+
+var heading = new Array();
+heading[0] = "secuencia"
+for( i = 1 ; i >= mayor;  i++  ){
+    heading[i] = "" + i
 }
-document.getElementById("tabla").innerHTML= t;
-} 
+
+
+var stock = new Array()
+stock[0] = new Array("valores de x", "88.625", "85.50", "85.81", "987")
+stock[1] = new Array("valores de y", "88.625", "85.50", "85.81", "989")
+stock[2] = new Array("valores de z", "88.625", "85.50", "85.81", "990")
+
+
+//COLUMNAS DE LA TABLA
+var tr = document.createElement('TR');
+tableBody.appendChild(tr);
+for (i = 0; i < heading.length; i++) {
+    var th = document.createElement('TH')
+    th.width = '75';
+    th.appendChild(document.createTextNode(heading[i]));
+    tr.appendChild(th);
+}
+
+//FILAS DE LA TABLA
+for (i = 0; i < stock.length; i++) {
+    var tr = document.createElement('TR');
+    for (j = 0; j < stock[i].length; j++) {
+        var td = document.createElement('TD')
+        td.appendChild(document.createTextNode(stock[i][j]));
+        tr.appendChild(td)
+    }
+    tableBody.appendChild(tr);
+}  
+myTableDiv.appendChild(table)
+}
 
 
 
